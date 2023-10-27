10675a6665617ba0c2710fbb2f17f1930b66fef9
Debasish Barai <debasishbaraiju@gmail.com>
2023-10-27 11:33:24 +0000

    bucket details changed

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const client = new S3Client ({
     region: 'ap-south-1',
     credentials: {
-        accessKeyId: 'AKIA6M4ZKZMBRWZDKQG3',
-        secretAccessKey: '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'
+        accessKeyId: 'AKIA6M4ZKZMB42LGZ46G',
+        secretAccessKey: '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'
     }
 })
 
 const putObject = async (fileName, contentType) => {
     const command = new PutObjectCommand ({
-        Bucket: 'discord-bucket-aws-clone',
+        Bucket: 'discord-clone-aws',
         Key: `/uploads/nodejs-example/${fileName}`,
         ContentType: contentType
     })
